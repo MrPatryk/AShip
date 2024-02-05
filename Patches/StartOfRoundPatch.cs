a5c1e767bc2578078bcf0ec96e7e32fc959905c6
MrPatryk <patrykbucholc@onet.pl>
2024-02-05 17:46:05 +0100

    force

--- Patches/StartOfRoundPatch.cs
+++ Patches/StartOfRoundPatch.cs
@@ -9,7 +9,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unity.Netcode;
+using Unity;
 using UnityEngine;
+using UnityEngine.Windows;
 
 namespace ShipPlusA.Patches
 {
@@ -19,6 +21,31 @@ namespace ShipPlusA.Patches
         public static bool HostCheck => NetworkManager.Singleton.IsHost || NetworkManager.Singleton.IsServer;
         public static List<GameObject> laser = new List<GameObject>();
 
+        public static bool moznaUzyc = true;
+        private static float czasOstatniegoUzycia;
+        public static float czasDoOdblokowania = 0f;
+        private static float countDown = 5f;
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void updateCountdown()
+        {
+            if (!moznaUzyc)
+            {
+                float czasOdOstatniegoUzycia = Time.time - czasOstatniegoUzycia;
+                czasDoOdblokowania = countDown - czasOdOstatniegoUzycia;
+
+                if (czasOdOstatniegoUzycia >= countDown)
+                {
+                    moznaUzyc = true;
+                }
+            }
+        }
+        public static void coundDown(float cd)
+        {
+            countDown = cd;
+            czasOstatniegoUzycia = Time.time;
+            moznaUzyc = false;
+        }
         [HarmonyPatch("openingDoorsSequence")]
         [HarmonyPostfix]
         static void refr(ref RoundManager __instance)
@@ -27,12 +54,29 @@ namespace ShipPlusA.Patches
             if(ShipPlusAMod.ShipModBase.checkShow()) ShipPlusAMod.ShipModBase.Logger.LogInfo(">>>>>>>>>>>>>>>check host");
             //if (HostCheck) return;
             if(ShipPlusAMod.ShipModBase.checkShow()) ShipPlusAMod.ShipModBase.Logger.LogInfo(">>>>>>>>>>>>>>>host ...");
+            __instance.StartCoroutine(reCreateLasers(__instance));
+        }
+        [HarmonyPatch("ShipHasLeft")]
+        [HarmonyPostfix]
+        static void refclose(ref RoundManager __instance)
+        {
+            if(ShipPlusAMod.ShipModBase.checkShow()) ShipPlusAMod.ShipModBase.Logger.LogInfo(">>>>>>>>>>>>>>>close door call");
+            //laser.Clear();
+        }
+        public static System.Collections.IEnumerator reCreateLasers(RoundManager __instance)
+        {
+            for(int i=0; i < laser.Count; i++)
+            {
+                GameObject.Destroy(laser[i]);
+            }
+            laser.Clear();
+            yield return new WaitForSeconds(0.2f);
             Transform existingChild = __instance.transform.Find("LightningScript");
 
-            if(ShipPlusAMod.ShipModBase.checkShow()) ShipPlusAMod.ShipModBase.Logger.LogInfo(">>>>>>>>>>>>>>>check");
+            if (ShipPlusAMod.ShipModBase.checkShow()) ShipPlusAMod.ShipModBase.Logger.LogInfo(">>>>>>>>>>>>>>>check");
             if (existingChild == null)
             {
-                if(ShipPlusAMod.ShipModBase.checkShow()) ShipPlusAMod.ShipModBase.Logger.LogInfo(">>>>>>>>>>>>>>>nie ma dziecka, tworze");
+                if (ShipPlusAMod.ShipModBase.checkShow()) ShipPlusAMod.ShipModBase.Logger.LogInfo(">>>>>>>>>>>>>>>no child, creating...");
                 for (int i = 0; i < ShipModBase.upgrades[ShipModBase.upgradeLevel].amount; i++)
                 {
                     GameObject childOb = i == 0 ? new GameObject("LightningScript") : new GameObject("LightningScript" + i);
@@ -44,16 +88,9 @@ namespace ShipPlusA.Patches
             }
             else
             {
-                if(ShipPlusAMod.ShipModBase.checkShow()) ShipPlusAMod.ShipModBase.Logger.LogInfo(">>>>>>>>>>>>>>>ma juz dziecko");
+                if (ShipPlusAMod.ShipModBase.checkShow()) ShipPlusAMod.ShipModBase.Logger.LogInfo(">>>>>>>>>>>>>>>had child, doing nothing...");
 
             }
-        }
-        [HarmonyPatch("ShipHasLeft")]
-        [HarmonyPostfix]
-        static void refclose(ref RoundManager __instance)
-        {
-            if(ShipPlusAMod.ShipModBase.checkShow()) ShipPlusAMod.ShipModBase.Logger.LogInfo(">>>>>>>>>>>>>>>close door call");
-            laser.Clear();
         }
     }
 }
